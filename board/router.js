dd9b10874f061499e715b4ac03f48df73b2497bf
LavanyaJay <lavanyajay75@gmail.com>
2019-11-20 17:29:40 +0100

    Add getBoard router

--- board/router.js
+++ board/router.js
@@ -1,16 +1,27 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = new Router();
-const Board = require('./model');
+const Board = require("./model");
 
-router.post('/:roomId/board', (req, res, next) => {
-	const board = {
-		roomId: req.params.roomId,
-	};
+router.post("/:roomId/board", (req, res, next) => {
+  const board = {
+    roomId: req.params.roomId
+  };
 
-	Board.create(board)
-		.then(board => res.json(board))
-		.catch(next);
+  Board.create(board)
+    .then(board => res.json(board))
+    .catch(next);
 });
 
+router.get("/:roomId/board", (req, res, next) => {
+  Board.findOne({
+    where: {
+      roomId: parseInt(req.params.roomId)
+    }
+  })
+    .then(board => {
+      res.json(board);
+    })
+    .catch(next);
+});
 
 module.exports = router;
